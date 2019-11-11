c8183cc6838c60a1574b741bbe81950ec5873aea
Irham Maulani <irhammaulani@gmail.com>
2019-11-12 06:26:37 +0800

    remove all component to router

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,13 @@
 <template>
   <v-app>
-    <component :is="component">
-      <router-view slot="body-content"></router-view>
-    </component>
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-
 export default {
   name: "app",
-  components: {
-    "navbar-header": Header
-  },
-  data() {
-    return {
-      component: "navbar-header"
-    };
-  }
+  created() {}
 };
 </script>
 

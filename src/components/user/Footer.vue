d8b47abb3822eb9035719613b0416692ea8efeed
Irham Maulani <irhammaulani@gmail.com>
2019-11-12 06:28:15 +0800

    add footer to user

---
+++ src/components/user/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="d-flex align-content-start flex-wrap">
+    <router-link v-for="footer in footers" class="pa-3 ma-0" :to="footer.url">
+      <p class="pa-0 ma-0 body-2">{{footer.text}}</p>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      footers: [
+        { text: "Help", url: "" },
+        { text: "About", url: "About" },
+        { text: "Terms", url: "" },
+        { text: "Careers", url: "" },
+        { text: "Privacy", url: "" }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.red {
+  background-color: red;
+}
+a {
+  text-decoration: none;
+  color: #8e8e8e !important;
+}
+a:hover {
+  text-decoration: underline;
+}
+</style>

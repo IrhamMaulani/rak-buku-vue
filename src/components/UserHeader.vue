9e2ed07d3caa2e921075fa64df9a568d0ece44b6
Irham Maulani <irhammaulani@gmail.com>
2019-11-15 18:47:46 +0800

    fix header layouting and add search, profile in header

--- src/components/UserHeader.vue
+++ src/components/UserHeader.vue
@@ -1,28 +1,44 @@
 <template>
   <div>
     <v-app-bar color="white" flat>
-      <v-content>
-        <v-container>
-          <v-layout align-center justify-center>
-            <v-flex xs12 md8>
-              <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
-
-              <!-- <v-spacer class="hidden-md-and-up"></v-spacer> -->
-              <h1 style="display:inline-block" class="mr-8">Rak Buku</h1>
-              <div class="hidden-sm-and-down" style="display:inline-block">
-                <router-link v-for="item in items" :key="item.title" :to="item.url">
-                  <v-btn text>
-                    <!-- <router-link :to="item.url">{{ item.title }}</router-link> -->
-                    {{ item.title }}
-                  </v-btn>
-                </router-link>
-              </div>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
+      <v-container :class="{'mt-12' : !$vuetify.breakpoint.smAndDown }">
+        <v-layout justify-center>
+          <v-flex xs12 lg8 md10 class="d-flex justify-start">
+            <v-toolbar-title>Rak Buku</v-toolbar-title>
+            <router-link
+              v-for="item in items"
+              :key="item.title"
+              :to="item.url"
+              class="hidden-sm-and-down"
+            >
+              <v-btn text>{{ item.title }}</v-btn>
+            </router-link>
+            <v-spacer></v-spacer>
+            <div>
+              <v-text-field solo flat hint placeholder="Search" class="hidden-sm-and-down"></v-text-field>
+            </div>
+            <v-btn icon class="hidden-sm-and-down">
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+            <v-menu offset-y class="ml-auto">
+              <template v-slot:activator="{ on }">
+                <v-button class="click-custom mt-2" v-on="on">
+                  <v-icon class>person</v-icon>
+                  <span class="mx-1">Irham</span>
+                  <v-icon class>keyboard_arrow_down</v-icon>
+                </v-button>
+              </template>
+              <v-list>
+                <v-list-item v-for="(item, index) in items" :key="index" @click>
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-app-bar>
-
     <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list-item>
         <!-- <v-list-item-avatar>
@@ -48,14 +64,15 @@
           </v-list-item>
         </v-list>
       </router-link>
+      <v-list-item>
+        <v-text-field flat hint placeholder="Search"></v-text-field>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-list-item>
     </v-navigation-drawer>
-    <v-container>
-      <v-layout align-center justify-center>
-        <v-flex xs12 md8>
-          <router-view></router-view>
-        </v-flex>
-      </v-layout>
-    </v-container>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -63,6 +80,12 @@
 export default {
   data() {
     return {
+      items: [
+        { title: "Click Me" },
+        { title: "Click Me" },
+        { title: "Click Me" },
+        { title: "Click Me 2" }
+      ],
       drawer: null,
       items: [
         { title: "Home", icon: "dashboard", url: "/" },
@@ -80,7 +103,13 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 a {
-  /* margin-left: 15px; */
   text-decoration: none;
+}
+.inline-block {
+  display: inline;
+}
+
+.click-custom {
+  cursor: pointer;
 }
 </style>

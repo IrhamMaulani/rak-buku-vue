e3418a6e6b1a7daf2dd4882b3e7b15dbe4be89bb
Irham Maulani <irhammaulani@gmail.com>
2019-11-16 17:39:29 +0800

    add book display in profile book section

--- src/components/user/ProfileBookItem.vue
+++ src/components/user/ProfileBookItem.vue
@@ -11,13 +11,57 @@
       >
         <v-hover v-slot:default="{ hover }">
           <v-card color="grey lighten-4" max-width="212" class="mx-auto mt-n2">
-            <v-img :aspect-ratio="1/1" src="https://cdn.vuetifyjs.com/images/cards/kitchen.png">
+            <v-img
+              :aspect-ratio="1/1"
+              height="190"
+              src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
+            >
               <v-expand-transition>
                 <div
                   v-if="hover"
-                  class="d-flex transition-fast-in-fast-out blue-grey darken-4 v-card--reveal display-3 white--text"
+                  class="d-flex justify-center transition-fast-in-fast-out blue-grey darken-4 v-card--reveal white--text"
                   style="height: 100%;"
-                >$14.99</div>
+                >
+                  <div class="overline justify-center text-center mt-2">
+                    <!-- <v-icon dark class="text-center">star_border</v-icon>6.9 -->
+                    <p class="text-center">Owned</p>
+                    <router-link :to="url">
+                      <p>Potter And Fucking Wizard</p>
+                    </router-link>
+
+                    <p>
+                      Penulis :
+                      <router-link :to="url">J.K Dunkey</router-link>
+                    </p>
+                    <p>Status : Dropped</p>
+                    <div class="d-flex flex-row justify-center">
+                      <v-tooltip bottom>
+                        <template v-slot:activator="{ on }">
+                          <p class="mr-3">
+                            <v-icon v-on="on" class="mr-1 white--text">star</v-icon>69
+                          </p>
+                        </template>
+                        <span>Your Score</span>
+                      </v-tooltip>
+                      <v-tooltip bottom>
+                        <template v-slot:activator="{ on }">
+                          <p class="mr-2">
+                            <v-icon v-on="on" class="mr-1 white--text">thumb_up</v-icon>
+                          </p>
+                        </template>
+                        <span>You Recomended It</span>
+                      </v-tooltip>
+                      <v-tooltip bottom>
+                        <template v-slot:activator="{ on }">
+                          <p>
+                            <v-icon v-on="on" class="mr-1 white--text">favorite_border</v-icon>
+                          </p>
+                        </template>
+                        <span>Your Favorite</span>
+                      </v-tooltip>
+                    </div>
+                  </div>
+                </div>
               </v-expand-transition>
             </v-img>
           </v-card>
@@ -28,7 +72,13 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      url: "/"
+    };
+  }
+};
 </script>
 
 <style scoped>
@@ -36,8 +86,15 @@ export default {};
   align-items: center;
   bottom: 0;
   justify-content: center;
-  opacity: 0.5;
+  opacity: 1;
   position: absolute;
   width: 100%;
+}
+a {
+  text-decoration: none;
+  color: white !important;
+}
+a:hover {
+  text-decoration: underline;
 }
 </style>

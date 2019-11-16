1dac29592564f579618c822329d4e6c94e947e6f
Irham Maulani <irhammaulani@gmail.com>
2019-11-16 20:18:39 +0800

    add profile and reviews url

---
+++ src/components/user/Review.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>adad</v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,25 @@ import AdminBook from './components/admin/book/BreadBook.vue'
 import Home from './components/user/Home.vue'
 import AdminHeader from "./components/AdminHeader.vue";
 import UserHeader from "./components/UserHeader.vue";
+import Profile from "./components/user/Profile.vue";
+import Review from "./components/user/Review.vue";
 
 export default [{
         path: '/',
         component: UserHeader,
         children: [{
-            path: "",
-            component: Home
-        }]
+                path: "",
+                component: Home
+            },
+            {
+                path: "profile",
+                component: Profile
+            },
+            {
+                path: "reviews",
+                component: Review
+            }
+        ]
     },
     {
         path: '/coba',

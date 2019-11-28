2a68e87f85a538e0c5d679869905ca38ca832668
Irham Maulani <irhammaulani@gmail.com>
2019-11-29 05:53:36 +0800

    add login modules

--- src/store/store.js
+++ src/store/store.js
@@ -3,15 +3,19 @@ import Vuex from 'vuex'
 import user from './modules/user'
 import role from './modules/role'
 import reputation from './modules/reputation'
+import book from './modules/book'
+import login from './modules/login'
 
 Vue.use(Vuex);
 
-export const store = new Vuex.Store({
+export default new Vuex.Store({
     // strict: true,
 
     modules: {
         user,
         role,
-        reputation
+        reputation,
+        book,
+        login
     }
 })

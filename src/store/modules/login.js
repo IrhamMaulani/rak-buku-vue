37edfb25837dae121e96d36d248cbec0e285ec2d
Irham Maulani <irhammaulani@gmail.com>
2019-11-29 05:53:54 +0800

    add login function

--- src/store/modules/login.js
+++ src/store/modules/login.js
@@ -3,8 +3,9 @@ import axios from "axios";
 export default {
     state: {
         status: '',
-        user: {},
-        token: localStorage.getItem('token') || ''
+        userName: localStorage.getItem('user_name') || '',
+        token: localStorage.getItem('token') || '',
+        message: "",
     },
     getters: {
         isLoggedIn: state => {
@@ -13,22 +14,38 @@ export default {
         authStatus: state => {
             return state.status
         },
+        authMessage: state => {
+            return state.message
+        },
+        userName: state => {
+            return state.userName
+        }
     },
     mutations: {
         auth_request(state) {
             state.status = 'loading'
         },
-        auth_success(state, token, user) {
+        auth_success(state, {
+            token,
+            user
+        }) {
+
             state.status = 'success'
             state.token = token
-            state.user = user
+            state.userName = user.user_name
+
         },
-        auth_error(state) {
+        auth_error(state, message) {
             state.status = 'error'
+            state.message = message
+        },
+        auth_failed(state) {
+            state.status = 'failed';
         },
         logout(state) {
             state.status = ''
             state.token = ''
+            state.userName = ''
         },
 
     },
@@ -39,22 +56,30 @@ export default {
             return new Promise((resolve, reject) => {
                 commit('auth_request')
                 axios({
-                        url: `${process.env.VUE_APP_API}book`,
+                        url: `${process.env.VUE_APP_API}login`,
                         data: user,
                         method: 'POST'
                     })
                     .then(resp => {
-                        const token = resp.data.token
+
+                        const token = `${resp.data.token_type} ${resp.data.access_token}`
                         const user = resp.data.user
+
                         localStorage.setItem('token', token)
+                        localStorage.setItem('user_name', user.user_name)
                         // Add the following line:
                         axios.defaults.headers.common['Authorization'] = token
-                        commit('auth_success', token, user)
+                        commit('auth_success', {
+                            token,
+                            user
+                        })
                         resolve(resp)
                     })
                     .catch(err => {
-                        commit('auth_error')
+
+                        commit('auth_error', err.response.data.message)
                         localStorage.removeItem('token')
+                        localStorage.removeItem('user_name')
                         reject(err)
                     })
             })

5aaeabced0f06902c0728631e67e9f1bbeff8de1
Irham Maulani <irhammaulani@gmail.com>
2019-11-29 01:54:20 +0800

    move all vue router to here and add meta requiresAuth

--- src/routes.js
+++ src/routes.js
@@ -8,57 +8,90 @@ import Profile from "./components/user/Profile.vue";
 import Review from "./components/user/Review.vue";
 import BookMenu from "./components/user/BookMenu.vue";
 import BookItem from "./components/user/BookItem.vue";
+import Login from "./components/auth/Login.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import store from './store/store.js'
 
-export default [{
-        path: '/',
-        component: UserHeader,
-        children: [{
-                path: "",
-                component: Home
-            },
-            {
-                path: "profile",
-                component: Profile
-            },
-            {
-                path: "reviews",
-                component: Review
-            },
-            {
-                path: "reviews/:id",
-                component: Review
-            },
-            {
-                path: "book",
-                component: BookMenu
-            },
 
-            {
-                path: "book/:id",
-                component: BookItem
-            },
-            {
-                path: "book/:id/reviews",
-                component: Review
-            }
-        ]
-    },
-    {
-        path: '/coba',
-        component: Modal
-    },
-    {
-        path: '/admin',
-        component: AdminHeader,
-        children: [{
-                path: 'add-user',
-                component: AdminUser
-            },
-            {
-                path: 'add-book',
-                component: AdminBook
-            }
-        ]
-    },
+Vue.use(Router)
 
-]
+const router = new Router({
+    mode: 'history',
+    routes: [{
+            path: '/',
+            component: UserHeader,
+            children: [{
+                    path: "",
+                    component: Home
+                },
+                {
+                    path: "profile",
+                    component: Profile,
+                    meta: {
+                        requiresAuth: true
+                    }
+
+                },
+                {
+                    path: "reviews",
+                    component: Review
+                },
+                {
+                    path: "reviews/:id",
+                    component: Review
+                },
+                {
+                    path: "book",
+                    component: BookMenu
+                },
+
+                {
+                    path: "book/:id",
+                    component: BookItem
+                },
+                {
+                    path: "book/:id/reviews",
+                    component: Review
+                },
+                {
+                    path: "login",
+                    component: Login
+                }
+            ]
+        },
+        {
+            path: '/coba',
+            component: Modal
+        },
+        {
+            path: '/admin',
+            component: AdminHeader,
+            children: [{
+                    path: 'add-user',
+                    component: AdminUser
+                },
+                {
+                    path: 'add-book',
+                    component: AdminBook
+                }
+            ]
+        },
+
+    ]
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+
+        if (store.getters.isLoggedIn) {
+            next()
+            return
+        }
+        next('/login')
+    } else {
+        next()
+    }
+})
+
+export default router

b28298d444a252639ee3d34c84c1096b48530301
Irham Maulani <irhammaulani@gmail.com>
2019-11-29 05:53:20 +0800

    fix store import

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import {
-  store
-} from './store/store';
+import store from './store/store.js'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css';
 import Routes from './routes'

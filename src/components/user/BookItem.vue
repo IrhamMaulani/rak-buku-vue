6827d184f24b4d49310ef3b10c7462e330fbcb2a
Irham Maulani <irhammaulani@gmail.com>
2019-12-01 21:33:33 +0800

    add data, addStatus, addBookmark

--- src/components/user/BookItem.vue
+++ src/components/user/BookItem.vue
@@ -1,50 +1,72 @@
 <template>
   <v-row class="mt-6">
     <v-col cols="12" md="7" lg="7">
-      <v-img
-        :src="require('../../assets/cover-book.jpg')"
-        lazy-src="https://picsum.photos/id/11/10/6"
-        height="825"
-      />
+      <div v-if="book.book_images_cover !== null">
+        <v-img
+          :src="book.book_images_cover.name"
+          lazy-src="https://picsum.photos/id/11/10/6"
+          height="825"
+        />
+      </div>
+      <div v-else>
+        <v-img
+          :src="require('../../assets/cover-book.jpg')"
+          lazy-src="https://picsum.photos/id/11/10/6"
+          height="825"
+        />
+      </div>
     </v-col>
 
     <v-col cols="12" md="5" lg="5" class>
       <v-row class="white mt-12">
         <v-col cols="6" class="border border-right text-center">
-          <p class="display-3 font-weight-black">69</p>
+          <p class="display-3 font-weight-black">{{book.score}}</p>
         </v-col>
         <v-col cols="6" class="border border-right">
           <p class="subtitle-1">
-            <router-link :to="url">Romance</router-link>,
-            <router-link :to="url">Manga</router-link>,
-            <router-link :to="url">Fucking Weaboo</router-link>
+            <router-link v-for="(tag, index) in book.tags" :key="tag.id" :to="url">
+              <span v-if="index != 0">{{', '}}</span>
+              <span>{{tag.name}}</span>
+            </router-link>
           </p>
         </v-col>
       </v-row>
-      <p class="display-2 font-weight-bold mt-12">The Lord Of The Rings The Return Of The King</p>
+      <p class="display-2 font-weight-bold mt-12">{{book.title}}</p>
 
       <v-row class="white mt-12">
         <v-col cols="6" class>
-          <p>Volume 1</p>
-          <p>Pengarang : J.K Dunkey</p>
-          <p>Publisher : Hogwart Lmao</p>
-          <p>Tahun Terbit : 2021</p>
-          <p>Origin Langauge : Zimbabwe</p>
+          <p>Vol {{book.volume}}</p>
+          <p>
+            Pengarang :
+            <router-link v-for="(author, index) in book.authors" :key="index" :to="url">
+              <span v-if="index != 0">{{', '}}</span>
+              <span>{{author.name}}</span>
+            </router-link>
+          </p>
+          <p>Publisher : {{book.publisher.name}}</p>
+          <p>Tahun Terbit : {{book.print_year}}</p>
+          <p>Origin Langauge : {{book.origin_language}}</p>
           <p class="title">IS OWNED</p>
 
-          <v-select :items="statuses" item-text="name" item-value="id" label="Wish List"></v-select>
-          <v-select :items="scores" item-text="id" item-value="id" label="Your Scores"></v-select>
+          <v-select
+            :items="statuses"
+            v-model="status"
+            item-text="name"
+            return-object
+            label="Status Read"
+            v-on:change="addStatus"
+          ></v-select>
+          <v-select
+            :items="scores"
+            v-model="userScore"
+            item-text="id"
+            item-value="id"
+            label="Your Scores"
+            v-on:change="addScore"
+          ></v-select>
         </v-col>
         <v-col cols="6" class>
-          <p class="body-2">
-            Eighth story arc of JoJo no Kimyou na Bouken series.
-            Following the Great East Japan earthquake,
-            Morioh has been stricken by vast earthen protrusions known by the locals as Wall Eyes. Despite investigation,
-            professional estimations are left empty. Meanwhile, the young girl Yasuho Hirose discovers a man buried within the ground,
-            he possesses a distinctive star-shaped birthmark together with deeply penetrating bite marks. The man, who has contracted
-            a staple case of amnesia is taken in by a local family and given the name Jousuke Higashikata,
-            however mysterious events cannot stop from piling one upon the other and soon that enigma known as stand emerges to
-          </p>
+          <p class="body-2">{{book.description}}</p>
         </v-col>
       </v-row>
     </v-col>
@@ -71,6 +93,9 @@ export default {
   components: {
     review: Review
   },
+  created() {
+    this.getData();
+  },
   data() {
     return {
       url: "",
@@ -93,8 +118,83 @@ export default {
         { id: 8 },
         { id: 9 },
         { id: 10 }
-      ]
+      ],
+      book: {
+        book_images_cover: {},
+        publisher: {}
+      },
+      slug: this.$route.params.id,
+      status: {},
+      userScore: {}
     };
+  },
+  methods: {
+    getData() {
+      this.$store.dispatch("setStatus", true);
+      this.$http
+        .get(`${this.$baseUrl}book/${this.slug}`)
+        .then(result => {
+          this.book = result.data;
+
+          let score = "";
+          let status = "";
+
+          if (result.data.user_score !== null) {
+            score = result.data.user_score.score;
+          }
+          if (result.data.check_bookmarked !== null) {
+            status = result.data.check_bookmarked.status;
+          }
+
+          this.statusAndScoreGetData(status, score);
+          this.$store.dispatch("setStatus", false);
+        })
+        .catch(error => {
+          alert(error);
+        });
+    },
+    statusAndScoreGetData(statusParam, scoreParam) {
+      const status = this.statuses.find(x => x.name === statusParam);
+      const score = this.scores.find(x => x.id === scoreParam);
+
+      this.status = status;
+      this.userScore = score;
+    },
+    addStatus() {
+      const data = {
+        status: this.status.name,
+        is_owned: 0,
+        book_id: this.book.id
+      };
+
+      this.$store.dispatch("setStatus", false);
+      this.$http
+        .post(`${process.env.VUE_APP_API}bookmark`, data)
+        .then(result => {
+          this.$store.dispatch("setStatus", true);
+          this.getData();
+        })
+        .catch(err => {
+          alert(error);
+        });
+    },
+    addScore() {
+      const data = {
+        score: this.userScore,
+        book_id: this.book.id
+      };
+      this.$store.dispatch("setStatus", false);
+
+      this.$http
+        .post(`${process.env.VUE_APP_API}score`, data)
+        .then(result => {
+          this.$store.dispatch("setStatus", true);
+          this.getData();
+        })
+        .catch(err => {
+          alert(error);
+        });
+    }
   }
 };
 </script>

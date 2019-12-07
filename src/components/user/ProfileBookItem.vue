d604f35a178b9570be0a4f295df9039c7ebabc08
Irham Maulani <irhammaulani@gmail.com>
2019-12-08 02:11:49 +0800

    add data from api

--- src/components/user/ProfileBookItem.vue
+++ src/components/user/ProfileBookItem.vue
@@ -4,72 +4,150 @@
       <!-- NOTE IF large 6 item md 9 -->
       <v-col
         cols="4"
-        v-for="n in 6"
-        :key="n"
+        v-for="(data,index) in datas.data"
+        :key="index"
         class="ma-0"
-        :class="{'mt-12' : $vuetify.breakpoint.lgOnly, 'mt-10' : $vuetify.breakpoint.mdOnly }"
+        :class="{
+          'mt-12': $vuetify.breakpoint.lgOnly,
+          'mt-10': $vuetify.breakpoint.mdOnly
+        }"
       >
         <v-hover v-slot:default="{ hover }">
           <v-card color="grey lighten-4" max-width="212" class="mx-auto mt-n2">
-            <v-img
-              :aspect-ratio="1/1"
-              height="190"
-              src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
-            >
-              <v-expand-transition>
-                <div
-                  v-if="hover"
-                  class="d-flex justify-center transition-fast-in-fast-out blue-grey darken-4 v-card--reveal white--text"
-                  style="height: 100%;"
-                >
-                  <div class="overline justify-center text-center mt-2">
-                    <!-- <v-icon dark class="text-center">star_border</v-icon>6.9 -->
-                    <p class="text-center">Owned</p>
-                    <router-link :to="url">
-                      <p>Potter And Fucking Wizard</p>
-                    </router-link>
+            <div v-if="data.book_images_cover !== null">
+              <v-img
+                :aspect-ratio="1 / 1"
+                height="190"
+                :src="`${imageUrl}storage/${data.book_images_cover.name}`"
+              >
+                <v-expand-transition>
+                  <div
+                    v-if="hover"
+                    class="d-flex justify-center transition-fast-in-fast-out blue-grey darken-4 v-card--reveal white--text"
+                    style="height: 100%;"
+                  >
+                    <div class="overline justify-center text-center mt-2">
+                      <!-- <v-icon dark class="text-center">star_border</v-icon>6.9 -->
+                      <p class="text-center">Owned</p>
+                      <router-link :to="url">
+                        <p>{{data.title}}</p>
+                      </router-link>
 
-                    <p>
-                      Penulis :
-                      <router-link :to="url">J.K Dunkey</router-link>
-                    </p>
-                    <p>Status : Dropped</p>
-                    <div class="d-flex flex-row justify-center">
-                      <v-tooltip bottom>
-                        <template v-slot:activator="{ on }">
-                          <p class="mr-3">
-                            <v-icon v-on="on" class="mr-1 white--text">star</v-icon>69
-                          </p>
-                        </template>
-                        <span>Your Score</span>
-                      </v-tooltip>
+                      <p>
+                        Penulis :
+                        <router-link :to="url">{{data.authors[0].name}}</router-link>
+                      </p>
+                      <p>Status : {{data.check_bookmarked.status}}</p>
+                      <div class="d-flex flex-row justify-center">
+                        <v-tooltip bottom>
+                          <template v-slot:activator="{ on }">
+                            <p class="mr-3">
+                              <v-icon v-on="on" class="mr-1 white--text">star</v-icon>
+                              {{data.user_score.score}}
+                            </p>
+                          </template>
+                          <span>Your Score</span>
+                        </v-tooltip>
 
-                      <v-tooltip bottom>
-                        <template v-slot:activator="{ on }">
-                          <p>
-                            <v-icon v-on="on" class="mr-1 white--text">favorite_border</v-icon>
-                          </p>
-                        </template>
-                        <span>Your Favorite</span>
-                      </v-tooltip>
+                        <v-tooltip bottom>
+                          <template v-slot:activator="{ on }">
+                            <p>
+                              <v-icon v-on="on" class="mr-1 white--text">favorite_border</v-icon>
+                            </p>
+                          </template>
+                          <span>Your Favorite</span>
+                        </v-tooltip>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </v-expand-transition>
-            </v-img>
+                </v-expand-transition>
+              </v-img>
+            </div>
+            <div v-else>
+              <v-img
+                :aspect-ratio="1 / 1"
+                height="190"
+                src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
+              >
+                <v-expand-transition>
+                  <div
+                    v-if="hover"
+                    class="d-flex justify-center transition-fast-in-fast-out blue-grey darken-4 v-card--reveal white--text"
+                    style="height: 100%;"
+                  >
+                    <div class="overline justify-center text-center mt-2">
+                      <!-- <v-icon dark class="text-center">star_border</v-icon>6.9 -->
+                      <p class="text-center">Owned</p>
+                      <router-link :to="url">
+                        <p>{{data.title}}</p>
+                      </router-link>
+
+                      <p>
+                        Penulis :
+                        <router-link :to="url">{{data.authors[0].name}}</router-link>
+                      </p>
+                      <p>Status : {{data.check_bookmarked.status}}</p>
+                      <div class="d-flex flex-row justify-center">
+                        <v-tooltip bottom>
+                          <template v-slot:activator="{ on }">
+                            <p class="mr-3">
+                              <v-icon v-on="on" class="mr-1 white--text">star</v-icon>
+                              {{data.user_score.score}}
+                            </p>
+                          </template>
+                          <span>Your Score</span>
+                        </v-tooltip>
+
+                        <v-tooltip bottom>
+                          <template v-slot:activator="{ on }">
+                            <p>
+                              <v-icon v-on="on" class="mr-1 white--text">favorite_border</v-icon>
+                            </p>
+                          </template>
+                          <span>Your Favorite</span>
+                        </v-tooltip>
+                      </div>
+                    </div>
+                  </div>
+                </v-expand-transition>
+              </v-img>
+            </div>
           </v-card>
         </v-hover>
       </v-col>
     </v-row>
+    <!-- <div class="text-center">
+      <v-pagination :length="4" circle></v-pagination>
+    </div>-->
   </div>
 </template>
 
 <script>
 export default {
+  created() {
+    this.getData();
+  },
   data() {
     return {
-      url: "/"
+      url: "/",
+      datas: [],
+      imageUrl: "http://localhost/rak-buku-web/public/"
     };
+  },
+  methods: {
+    getData() {
+      this.$store.dispatch("setStatus", true);
+      this.$http
+        .get(`${this.$baseUrl}bookmark`)
+        .then(result => {
+          this.datas = result.data;
+          this.$store.dispatch("setStatus", false);
+        })
+        .catch(error => {
+          alert(error);
+          this.$store.dispatch("setStatus", false);
+        });
+    }
   }
 };
 </script>

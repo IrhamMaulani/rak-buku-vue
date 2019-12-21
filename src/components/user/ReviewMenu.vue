426347966563aba144b5ceeb561b70cfaf799d5b
Irham Maulani <irhammaulani@gmail.com>
2019-12-21 20:58:31 +0800

    add reviewMenu component

---
+++ src/components/user/ReviewMenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <review-book :detail="detail" />
+</template>
+
+<script>
+import ReviewBook from "../user/ReviewBookContent";
+export default {
+  components: {
+    ReviewBook: ReviewBook
+  },
+  data() {
+    return {
+      detail: {
+        url: "review?userIncluded=0&",
+        isUser: 1
+      }
+    };
+  }
+};
+</script>
+
+<style>
+</style>

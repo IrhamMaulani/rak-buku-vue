73825714cbe340800a935ab7545a5bef4a53ba37
Irham Maulani <irhammaulani@gmail.com>
2019-12-25 05:48:21 +0800

    add about

---
+++ src/components/user/About.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app class="white mt-5">
+    <v-content>
+      <v-container class="mt-12" fluid>
+        <v-card class="elevation-12">
+          <v-card-title>About Rak Buku</v-card-title>
+          <v-card-text class="headline">
+            <p>
+              There are many variations of passages of Lorem Ipsum available,
+              but the majority have suffered alteration in some form, by injected humour,
+              or randomised words which don't look even slightly believable.
+              If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.
+              All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.
+              It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.
+              The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+            </p>
+
+            <p>
+              On the other hand, we denounce with righteous indignation and dislike men who are so
+              beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire,
+              that they cannot foresee the pain and trouble that are bound to ensue;
+              and equal blame belongs to those who fail in their duty through weakness of will, which
+              is the same as saying through shrinking from toil and pain.
+              These cases are perfectly simple and easy to distinguish. In a free hour, when our
+              power of choice is untrammelled and when nothing prevents our being able to do what we like best,
+              every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims
+              of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted.
+              The wise man therefore always holds in these matters to this principle of selection:
+              he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

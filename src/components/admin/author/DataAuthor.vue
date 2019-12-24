6e8896628ed0ed4cf96f53d5c7e538f2150057b7
Irham Maulani <irhammaulani@gmail.com>
2019-12-24 23:40:53 +0800

    add item-per-page for author

--- src/components/admin/author/DataAuthor.vue
+++ src/components/admin/author/DataAuthor.vue
@@ -15,7 +15,7 @@
         class="elevation-1"
         :loading="loading.loading"
         :loading-text="loading.message"
-        items-per-page="6"
+        :items-per-page="5"
       >
         <template v-slot:item.author_image.name="{ item }">
           <span v-if="item.author_image !== null">
